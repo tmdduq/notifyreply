4b312168da117527a3cf83436f987e737b6323e9
dhtmdduq <dhtmdduq@hotmail.com>
2021-11-20 21:47:31 +0900

    add Stock

--- app/src/main/java/com/osy/callapi/ApiCoin.java
+++ app/src/main/java/com/osy/callapi/ApiCoin.java
@@ -23,8 +23,8 @@ public class ApiCoin {
     protected Map<String, String> coinCodeMap_UPBIT;
 
     //빗썸은 전체목록을 보여주는 JSON이 따로 없네..
-    final String[] coinList_BITHUMB = new String[]{"비트코인","리플","이더리움","폴카닷","스텔라루멘","도지코인","클레이튼","유니스왑","라이트코인","위믹스","마일벌스","알파체인","링크플로우","퀴즈톡","어셈블프로토콜","바이낸스코인","콘텐토스","오미세고","펑션엑스","이오스","에이다","이캐시","제노토큰","디비전","솔라나","이더리움 클래식","쿼크체인","앵커뉴럴월드","루나","트론","미스블록","프로톤","퀀텀","소다코인","체인링크","비체인","비트코인 캐시","왐토큰","하이브","엑시인피니티","테조스","코스모스","비트토렌트","벨라프로토콜","웨이브","폴라리스 쉐어","어댑터 토큰","알파쿼크","블로서리","아이젝","코르텍스","연파이낸스","메탈","벨로프로토콜","베라시티","비트코인 골드","아픽스","밀크","알고랜드","쿠사마","이브이지","스와이프","오르빗 체인","샌드박스","아르고","아이온","버거스왑","비트코인에스브이","무비블록","세럼","팬케이크스왑","바이프로스트","베이직","에이치닥","룸네트워크","믹스마블","쎄타토큰","메타디움","왁스","고머니2","너보스","마이네이버앨리스","칠리즈","비트코인 다이아몬드","페이코인","밀리미터토큰","온톨로지가스","넴","디센트럴랜드","아이콘","보라","리니어파이낸스","지엑스체인","라이브피어","심볼","스팀","애니버스","바이오패스포트","펀디엑스","셀러네트워크","누사이퍼","썸씽","질리카","크로미아","맵프로토콜","엘리시아","렌","신세틱스","엔진코인","파워렛저","옵저버","피르마체인","위드","플레타","비너스","카이버 네트워크","엘프","트루체인","웨이키체인","오브스","크립토닷컴체인","스시스왑","스트라티스","컴파운드","온톨로지","폴리곤","네스트리","머신익스체인지코인","이오스트","카르테시","저스트","싸이클럽","이마이너","링엑스","썬","코넌","베이커리토큰","골렘","게이머코인","스테이터스네트워크토큰","쎄타퓨엘","더마이다스터치골드","리저브라이트","우마","뉴메레르","어거","베이직어텐션토큰","다이","아모코인","제로엑스","오키드","에이브","콜라토큰","300피트 네트워크","밸런서","보아","타키온프로토콜","힙스","템코","더그래프","써틱","앵커","메이커","뱅코르","라이파이낸스","울트라","미러 프로토콜","브이시스템즈","루프링","다드","센트럴리티","아로와나토큰","월튼체인","오션프로토콜","트러스트버스","에이아이워크","이포스","랠리","밸러토큰","에이피엠 코인"};
-    final String[] coinCodeList_BITHUMB = new String[]{"BTC","XRP","ETH","DOT","XLM","DOGE","KLAY","UNI","LTC","WEMIX","MVC","ARPA","LF","QTCON","ASM","BNB","COS","OMG","FX","EOS","ADA","XEC","XNO","DVI","SOL","ETC","QKC","ANW","LUNA","TRX","MSB","XPR","QTUM","SOC","LINK","VET","BCH","WOM","HIVE","AXS","XTZ","ATOM","BTT","BEL","WAVES","POLA","ADP","AQT","BLY","RLC","CTXC","YFI","MTL","VELO","VRA","BTG","APIX","MLK","ALGO","KSM","EVZ","SXP","ORC","SAND","AERGO","AION","BURGER","BSV","MBL","SRM","CAKE","BFC","BASIC","HDAC","LOOM","MIX","THETA","META","WAXP","GOM2","CKB","ALICE","CHZ","BCD","PCI","MM","ONG","XEM","MANA","ICX","BORA","LINA","GXC","LPT","XYM","STEEM","ANV","BIOT","PUNDIX","CELR","NU","SSX","ZIL","CHR","MAP","EL","REN","SNX","ENJ","POWR","OBSR","FCT","WIKEN","FLETA","XVS","KNC","ELF","TRUE","WICC","ORBS","CRO","SUSHI","STRAX","COMP","ONT","MATIC","EGG","MXC","IOST","CTSI","JST","CYCLUB","EM","RINGX","SUN","CON","BAKE","GLM","GHX","SNT","TFUEL","TMTG","RSR","UMA","NMR","REP","BAT","DAI","AMO","ZRX","OXT","AAVE","COLA","FIT","BAL","BOA","IPX","HIBS","TEMCO","GRT","CTK","ANKR","MKR","BNT","RAI","UOS","MIR","VSYS","LRC","DAD","CENNZ","ARW","WTC","OCEAN","TRV","AWO","WOZX","RLY","VALOR","APM"};
+    final String[] coinList_BITHUMB = new String[]{"비트코인", "위믹스 ", "이더리움", "보라", "어셈블프로토콜", "리플 ", "샌드박스", "디센트럴랜드", "디비전 ", "파워렛저", "바이프로스트", "엔진코인 ", "싸이클럽", "이브이지 ", "제노토큰", "링크플로우", "왁스", "크로미아", "솔라나", "템코", "믹스마블", "클레이튼 ", "300피트 네트워크 ", "퀴즈톡 ", "페이코인", "콜라토큰", "루프링", "도지코인", "미스블록", "애니버스 ", "마이네이버앨리스", "에이다", "트론 ", "아르고", "라이트코인", "앵커뉴럴월드 ", "퀀텀", "하이브", "이더리움 클래식", "소다코인 ", "라이브피어", "이캐시", "이오스", "마일벌스", "폴카닷", "폴라리스 쉐어", "밀크", "알고랜드 ", "알파쿼크", "오미세고", "썸씽", "쿼크체인", "루나 ", "무비블록", "웨이키체인", "스텔라루멘", "비체인", "스와이프", "너보스", "라이파이낸스", "콘텐토스", "바이오패스포트", "체인링크", "게이머코인", "칠리즈", "바이낸스코인", "아로와나토큰 ", "크립토닷컴체인", "엘리시아 ", "비트토렌트", "베이직어텐션토큰", "룸네트워크", "엑시인피니티 ", "앵커", "오르빗 체인", "어댑터 토큰", "비트코인 캐시", "카르테시", "베라시티", "베이직", "코스모스", "아픽스", "테조스", "폴리곤 ", "밀리미터토큰", "골렘", "센트럴리티", "위드", "오키드", "누사이퍼", "왐토큰", "에이치닥", "아이콘 ", "오브스 ", "연파이낸스", "힙스 ", "울트라", "신세틱스", "아모코인", "엘프", "메타디움", "링엑스", "온톨로지", "스트라티스", "온톨로지가스", "유니스왑", "비트코인 골드", "프로톤", "더마이다스터치골드", "에이아이워크", "세럼", "넴", "코르텍스", "펀디엑스", "펑션엑스", "셀러네트워크", "아이온", "메탈", "보아", "비트코인에스브이", "코넌 ", "오션프로토콜", "스팀", "플레타", "리니어파이낸스", "트러스트버스", "블로서리", "렌", "썬", "버거스왑", "리저브라이트", "벨로프로토콜", "맵프로토콜", "카이버 네트워크", "이마이너 ", "쎄타토큰", "트루체인 ", "이오스트 ", "더그래프", "고머니2", "이포스", "우마", "저스트", "알파체인", "피르마체인", "옵저버", "스테이터스네트워크토큰", "비트코인 다이아몬드", "지엑스체인", "제로엑스", "심볼", "타키온프로토콜 ", "랠리", "밸러토큰", "에이피엠 코인", "다드", "미러 프로토콜", "밸런서", "다이", "쎄타퓨엘", "아이젝", "스시스왑", "질리카", "브이시스템즈 ", "어거", "쿠사마", "에이브", "팬케이크스왑 ", "네스트리", "월튼체인", "웨이브", "베이커리토큰", "비너스", "컴파운드", "뱅코르", "뉴메레르", "메이커", "써틱", "벨라프로토콜 ", "머신익스체인지코인"};
+    final String[] coinCodeList_BITHUMB = new String[]{"BTC", "WEMIX", "ETH", "BORA", "ASM", "XRP", "SAND", "MANA", "DVI", "POWR", "BFC", "ENJ", "CYCLUB", "EVZ", "XNO", "LF", "WAXP", "CHR", "SOL", "TEMCO", "MIX", "KLAY", "FIT", "QTCON", "PCI", "COLA", "LRC", "DOGE", "MSB", "ANV", "ALICE", "ADA", "TRX", "AERGO", "LTC", "ANW", "QTUM", "HIVE", "ETC", "SOC", "LPT", "XEC", "EOS", "MVC", "DOT", "POLA", "MLK", "ALGO", "AQT", "OMG", "SSX", "QKC", "LUNA", "MBL", "WICC", "XLM", "VET", "SXP", "CKB", "RAI", "COS", "BIOT", "LINK", "GHX", "CHZ", "BNB", "ARW", "CRO", "EL", "BTT", "BAT", "LOOM", "AXS", "ANKR", "ORC", "ADP", "BCH", "CTSI", "VRA", "BASIC", "ATOM", "APIX", "XTZ", "MATIC", "MM", "GLM", "CENNZ", "WIKEN", "OXT", "NU", "WOM", "HDAC", "ICX", "ORBS", "YFI", "HIBS", "UOS", "SNX", "AMO", "ELF", "META", "RINGX", "ONT", "STRAX", "ONG", "UNI", "BTG", "XPR", "TMTG", "AWO", "SRM", "XEM", "CTXC", "PUNDIX", "FX", "CELR", "AION", "MTL", "BOA", "BSV", "CON", "OCEAN", "STEEM", "FLETA", "LINA", "TRV", "BLY", "REN", "SUN", "BURGER", "RSR", "VELO", "MAP", "KNC", "EM", "THETA", "TRUE", "IOST", "GRT", "GOM2", "WOZX", "UMA", "JST", "ARPA", "FCT", "OBSR", "SNT", "BCD", "GXC", "ZRX", "XYM", "IPX", "RLY", "VALOR", "APM", "DAD", "MIR", "BAL", "DAI", "TFUEL", "RLC", "SUSHI", "ZIL", "VSYS", "REP", "KSM", "AAVE", "CAKE", "EGG", "WTC", "WAVES", "BAKE", "XVS", "COMP", "BNT", "NMR", "MKR", "CTK", "BEL", "MXC"};
 
     public ApiCoin(){
         getBithumbList();
@@ -108,12 +108,7 @@ public class ApiCoin {
             double sp = ((JSONObject)mainArray.get(0)).getDouble("opening_price");
             double cp = ((JSONObject)mainArray.get(0)).getDouble("trade_price");
             //double ripple24 = mainObject.getDouble("fluctate_rate_24H");
-            double rippleRage = 0;
-            if(sp<cp)
-                rippleRage = cp/sp;
-            else
-                rippleRage = sp/cp *(-1);
-
+            double rippleRage = (cp-sp) / sp * 100;
             String curPrice =  numberFormat.format(cp);
             String t = curPrice+"원("+numberFormat.format(rippleRage)+"%)";
             return t;
@@ -143,11 +138,7 @@ public class ApiCoin {
             double sp = dataObject.getDouble("opening_price");
             double cp = dataObject.getDouble("closing_price");
             double ripple24 = dataObject.getDouble("fluctate_rate_24H");
-            double rippleRage = 0;
-            if(sp<cp)
-                rippleRage = cp/sp;
-            else
-                rippleRage = sp/cp *(-1);
+            double rippleRage = (cp-sp) / sp *100;
 
             String curPrice =  numberFormat.format(dataObject.getDouble("closing_price"));
 
@@ -183,9 +174,7 @@ public class ApiCoin {
                 double sp = ((JSONObject)mainArray.get(i)).getDouble("opening_price");
                 double cp = ((JSONObject)mainArray.get(i)).getDouble("trade_price");
                 //double ripple24 = mainObject.getDouble("fluctate_rate_24H");
-                double rippleRage = 0;
-                if(sp<cp) rippleRage = cp/sp;
-                else rippleRage = sp/cp *(-1);
+                double rippleRage = (cp-sp) / sp *100;
 
                 String curPrice =  numberFormat.format(cp);
                 sb.append(coinCodeMap_UPBIT.get(name)+":"+curPrice+"원("+numberFormat.format(rippleRage)+"%)\n");
@@ -223,9 +212,7 @@ public class ApiCoin {
                 JSONObject dataObject = allDataObject.getJSONObject(coinCode);
                 double sp = dataObject.getDouble("opening_price");
                 double cp = dataObject.getDouble("closing_price");
-                double rippleRage = 0;
-                if(sp<cp) rippleRage = cp/sp;
-                else rippleRage = sp/cp *(-1);
+                double rippleRage = (cp-sp) / sp * 100;
                 String curPrice =  numberFormat.format(cp);
                 sb.append(coinCodeMap_BITHUMB.get(coinCode)+":"+curPrice+"원("+numberFormat.format(rippleRage)+"%)\n");
             }
